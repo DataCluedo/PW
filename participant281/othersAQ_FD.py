aa542a65b6fcc52f474d029fc8c2cf056ee10e3e
meettanmaysinha <tanmay.sinha655@gmail.com>
2019-10-07 09:56:19 +0200

    adding all pid

---
+++ participant281/othersAQ_FD.py
@@ -0,0 +1,9 @@
+def different_idea(task,level):
+
+	if (task=="AQ" and level=="FD1"):
+		idea="Matplotlib has a handy function to generate bar chart: https://matplotlib.org/api/_as_gen/matplotlib.pyplot.bar.html. If this idea changes measures you designed earlier or inspires you to create a new measure, try again. In case the idea information is not helpful and you are not sure if/how you might design new measures (or, revise measures you already designed), you can ask for a different idea by calling the function different_idea(\"AQ\",\"FD2\")."
+
+	if (task=="AQ" and level=="FD2"):
+		idea="Matplotlib has handy a function to generate 2-D histogram: https://matplotlib.org/api/_as_gen/matplotlib.pyplot.hist2d.html. If this idea changes measures you designed earlier or inspires you to create a new measure, try again."
+
+	return(idea)
